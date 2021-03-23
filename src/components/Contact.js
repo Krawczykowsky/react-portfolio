1e809d6aa126b7d0601c2fde22b00a38abd4eee0
Maciej <krawczykowski.maciej@gmail.com>
2021-03-24 00:38:01 +0100

    axios get

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,18 @@ import db from './db.json'
 
 const axios = require('axios');
 
-
+axios.get('http://localhost:5000/messages')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
 
 const Contact = () => {
     return (
